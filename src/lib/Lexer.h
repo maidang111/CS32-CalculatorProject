c493556309cd602179452fe2ef55450473c57df1
“Mai Dang” <maidang0001@gmail.com>
2023-10-15 13:31:10 -0700

    fixed make file

--- src/lib/Lexer.h
+++ src/lib/Lexer.h
@@ -7,16 +7,15 @@ using namespace std;
 
 class Lexer{
     public:
-    vector <char> user_input;
+    // vector <char> user_input;
 
-    string user_input;
-    vector <string> whole_input;
-    set <char> possible_values;
-    vector <Token*> tokens;
-    ~Lexer();
+    // string user_input;
+    // vector <string> whole_input;
+    // set <char> possible_values;
+    // vector <Token*> tokens;
+    // ~Lexer();
     Lexer();
-
-
-    void create_tokens();
-    void print_tokens();
+    
+    // void create_tokens();
+    // void print_tokens();
 };
