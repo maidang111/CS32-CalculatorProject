1ddd99cc9cd2a6b321cf64c2477a302ac86c2b84
“Mai Dang” <maidang0001@gmail.com>
2023-10-31 20:51:54 -0700

    done

--- src/lib/InfixParser.cpp
+++ src/lib/InfixParser.cpp
@@ -29,10 +29,6 @@ void InfixParser::build_AST(){
             cout << endl;
             cout << AST->get_value() << endl;
             ASTheads.push_back(AST);
-
-            for (size_t i = 0; i < variables.size(); i++){
-                cout << variables.at(i)->value << variables.at(i)->raw_value << endl;
-            }
         } 
     }
     for(size_t i = 0; i < ASTheads.size(); i++){
