dff645a38226bd241d07c43b9595151f1a097c9c
“Mai Dang” <maidang0001@gmail.com>
2023-10-31 21:21:54 -0700

    done

--- src/lib/InfixParser.cpp
+++ src/lib/InfixParser.cpp
@@ -23,7 +23,6 @@ void InfixParser::build_AST(){
             AST = parseEqual();
             if (nextToken->raw_value != "END"){
             cout << "next token isn't END token: " << nextToken->raw_value;
-            exit(1);
             } 
             AST->print();
             cout << endl;
@@ -55,7 +54,6 @@ Token* InfixParser::parseEqual(){
     while (true){
         if (nextToken == nullptr){
             cout << "null expression" << endl;
-            exit(1);
         } else if(nextToken->raw_value == "="){
             scanToken();
             Token* equal1 = parseEqual();
@@ -87,7 +85,6 @@ Token* InfixParser::parseExpression(){
     while (true){
         if (nextToken == nullptr){
             cout << "null expression" << endl;
-            exit(1);
         } else if(nextToken->raw_value == "+"){
             scanToken();
             Token* term1 = parseTerm();
@@ -113,7 +110,6 @@ Token* InfixParser::parseTerm(){
     while (true){
         if (nextToken == nullptr){
             cout << "null expression" << endl;
-            exit(1);
         } else if(nextToken->raw_value == "*"){
             scanToken();
             Token* factor1 = parseFactor();
