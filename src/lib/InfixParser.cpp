8dd5e84343a1c8122c10807b95dc1ed274e40274
Sungchae Park <spark686@ucsb.edu>
2023-11-02 15:13:46 -0700

    m

--- src/lib/InfixParser.cpp
+++ src/lib/InfixParser.cpp
@@ -84,7 +84,7 @@ bool InfixParser::error_assignment(size_t index) {
         // (a =) case, no right value
         if (tokens.at(i)->raw_value == ")") {
             if (i > 0) {
-                if (tokens.at(i - 1)->raw_value == "=" || tokens.at(i - 1) == "(") {
+                if (tokens.at(i - 1)->raw_value == "=" || tokens.at(i - 1)->raw_value == "(") {
                     is_error = true;
                     last_error = tokens.at(i);
                 }
