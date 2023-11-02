80df72780a19cbe65dc3870a38babdd2e919efb7
Sungchae Park <spark686@ucsb.edu>
2023-11-02 12:06:58 -0700

    m

--- src/lib/InfixParser.cpp
+++ src/lib/InfixParser.cpp
@@ -40,7 +40,7 @@ bool InfixParser::error_parenthesis(size_t index) {
             cout << "Unexpected token at line " << tokens.at(i)->row << " column " << tokens.at(i)->column << ": " << tokens.at(i)->raw_value << endl;
         }
         if (tokens.at(i)->raw_value == "END") {
-            if (num_parenthesis != 0) {
+            if (num_parenthesis > 0) {
                 error_parenthesis = true;
                 cout << "Unexpected token at line " << tokens.at(i)->row << " column " << tokens.at(i)->column << ": " << tokens.at(i)->raw_value << endl;
             }
