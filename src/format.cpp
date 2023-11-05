68949d7c25c0edb41ed2d99f9f6a0fecc36eeb62
“Mai Dang” <maidang0001@gmail.com>
2023-11-05 14:31:38 -0800

    added print statements

--- src/format.cpp
+++ src/format.cpp
@@ -6,6 +6,12 @@
 using namespace std; 
 
 int main(){
-    
+    string line = "";
+    while (!cin.eof()){
+        getline(cin, line);
+        cout << line << endl;
+        // for testing
+        // cout << line << endl;
+    }
     return 0;
 }
--- src/lib/Scrypter.h
+++ src/lib/Scrypter.h
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Scrypter.h"
 using namespace std;
 
 class Scrypter{
