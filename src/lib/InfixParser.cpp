7311aac948c649f5819c2dff01ea729f1b81897f
Sungchae Park <sungchaepark@169-231-73-222.wireless.ucsb.edu>
2023-11-09 14:47:18 -0800

    read_token single case small change

--- src/lib/InfixParser.cpp
+++ src/lib/InfixParser.cpp
@@ -128,7 +128,9 @@ void InfixParser::read_token(bool calc) {
     // cout << "currIndex3: " << curr_index << endl;
     if (index == curr_index) {
         AST_Node* v = single_value_token(index);
-        ASTs.push_back(v);
+        if (!v) {
+            ASTs.push_back(v);
+        }
         index = curr_index + 2;
         return; 
     }
