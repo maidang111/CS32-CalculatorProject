8713ac357dbc8608818d672251dea69334b573ac
Sungchae Park <sungchaepark@169-231-73-222.wireless.ucsb.edu>
2023-11-08 16:20:49 -0800

    gradescope testcases view

--- src/calc.cpp
+++ src/calc.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "lib/Lexer.h"
 #include "lib/InfixParser.h"
+#include "lib/AST.h"
 #include <string>
 
 using namespace std; 
@@ -8,6 +9,9 @@ using namespace std;
 int main(){
     Lexer a;
     a.create_endtokens();
+    for (size_t i = 0; i < a.multi_end_tokens.size(); ++i) {
+        cout << a.multi_end_tokens.at(i)->raw_value << endl;
+    }
     InfixParser infixParser(a.multi_end_tokens);
     infixParser.read_all_token();
     infixParser.print_all();
