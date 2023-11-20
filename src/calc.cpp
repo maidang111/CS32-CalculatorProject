6ef96bde6942c86538d79cbfc73347079e3b302a
Sungchae Park <sungchaepark@169-231-75-211.wireless.ucsb.edu>
2023-11-19 16:20:23 -0800

    testcases in the gradescope

--- src/calc.cpp
+++ src/calc.cpp
@@ -10,7 +10,15 @@ int main(){
     Lexer a;
     a.create_endtokens();
     InfixParser infixParser(a.multi_end_tokens);
-    infixParser.read_all_token(true);
-    a.delete_endtokens();
+    // infixParser.read_all_token(true);
+    // a.delete_endtokens();
+    for (size_t i = 0; i < infixParser.tokens.size(); ++i) {
+        if (infixParser.tokens.at(i)->raw_value == "END") {
+            cout << endl;
+        }
+        else {
+            cout << infixParser.tokens.at(i)->raw_value;
+        }
+    }
     return 0;
 }
