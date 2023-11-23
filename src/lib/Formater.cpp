e0dda9405064f7120acea02a9db08c3b4fbd792a
“Mai Dang” <maidang0001@gmail.com>
2023-11-22 21:33:39 -0800

    redo

--- src/lib/Formater.cpp
+++ src/lib/Formater.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -27,52 +26,20 @@ void Formater::deleteStatements(){
 }
 
 void Formater::buildASTs(){
-     while(index != tokens.size()){
+    while(index != tokens.size()){
+        level = 0;
         if(tokens.at(index)->raw_value == "END"){
             index++;
         } else {
-            if(tokens.at(index)->is_function){
-                level = 0;
-                if (tokens.at(index)->raw_value == "foo"){
-                }
-                FunctionCall* root = buildFunction();
-                if (root != nullptr){
-                    ASTHeads.push_back(root);
-                }
-            } else {
-                if (tokens.at(index)->raw_value == "foo"){
-                }
-                level = 0;
-                Statement* root = buildAST();
-                if (root != nullptr){
-                    ASTHeads.push_back(root);
-                }
+            Statement* root = buildAST();
+            if (root != nullptr){
+                ASTHeads.push_back(root);
             }
+            // root->print()
+            // ASTHeads.at(0)->print();
         }
     }
 }
-
-FunctionCall* Formater::buildFunction(){
-    FunctionCall* functionCall = new FunctionCall();
-    // functionCall->is_fuction_def = false;
-    functionCall->functionName = tokens.at(index)->raw_value;
-    // cout << tokens.at(index)->raw_value << endl;
-    index += 2;
-    while(tokens.at(index)->raw_value != ")"){
-    if (tokens.at(index)->raw_value != ","){
-        // cout << tokens.at(index)->raw_value << endl;
-        functionCall->parameters.push_back(tokens.at(index));
-    }
-    index++;
-    }
-    // for(size_t i = 0; i < ASTFunctions.size(); i++){
-    //     if (ASTFunctions.at(i)->functionName == functionCall->functionName){
-    //         functionCall->function = ASTFunctions.at(i);
-    //     }
-    // }
-    return functionCall;
-}
-
 
 Statement* Formater::buildAST(){
     if(tokens.at(index)->raw_value == "def"){
@@ -218,11 +185,6 @@ Statement* Formater::buildAST(){
         Print* printBlock = new Print();
         printBlock->level = level;
         size_t tempLevel = level;
-        if(tokens.at(index)->is_function){
-            printBlock->body.push_back(buildFunction());
-        } else {
-            printBlock->body.push_back(buildAST());
-        }
         level = 0;
         printBlock->body.push_back(buildAST());
         level = tempLevel;
